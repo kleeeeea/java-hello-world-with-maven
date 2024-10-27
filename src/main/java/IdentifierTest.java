efab0eb6e7a2af318e5de174deb6ed5d3eb1b01c
kleeeeea <klee@cs.ucsb.edu>
2024-10-27 16:26:41 -0700

    fix

---
+++ src/main/java/IdentifierTest.java
@@ -0,0 +1,73 @@
+/*
+���Ա�ʶ����ʹ��
+
+1. ʲô�Ǳ�ʶ����Java�б��������������Ҫ������ʱʹ�õ��ַ����У���Ϊ��ʶ����
+
+���ɣ������Լ����������ֵĵط����б�ʶ�������磺������������������������������������
+
+2. ��ʶ������������ (����Ҫ���أ������򣬱��벻ͨ��)
+> ��26��Ӣ����ĸ��Сд��0-9 ��_�� $ ���
+> ���ֲ����Կ�ͷ��
+> ������ʹ�ùؼ��ֺͱ����֣����ܰ����ؼ��ֺͱ����֡�
+> Java���ϸ����ִ�Сд�����������ơ�
+> ��ʶ�����ܰ����ո�
+
+3.��ʶ���������淶(�������ء���������أ���������ж�������ִ�С�ֻ�����ױ��˱���)
+> �������൥�����ʱ������ĸ��Сд��xxxyyyzzz��
+  ���磺java.lang��com.atguigu.bean
+  
+> �������ӿ������൥�����ʱ�����е��ʵ�����ĸ��д��XxxYyyZzz
+  ���磺HelloWorld��String��System��
+  
+> �����������������൥�����ʱ����һ����������ĸСд���ڶ������ʿ�ʼÿ����������ĸ��д��xxxYyyZzz
+  ���磺age,name,bookName,main,binarySearch,getName
+  
+> ��������������ĸ����д���൥��ʱÿ���������»������ӣ�XXX_YYY_ZZZ
+  ���磺MAX_VALUE,PI,DEFAULT_CAPACITY
+
+
+
+˵��������ڶ����ʶ��ʱ��Ҫע�⡰����֪�⡱
+*/
+class IdentifierTest{
+	public static void main(String[] args){
+		
+		int abc = 12;
+		int age = 12; //age :��ʶ��
+
+
+		char gender = 'a';
+
+		char c1 = 'Ů';
+		
+		//���Ƽ���д��
+		//int myage = 12;
+
+		//System.out.println(myage);
+
+		int myAge = 12;
+		
+	}
+
+//	public static void main1(String[] args){
+//
+//	}
+}
+
+
+class _a$bc{
+}
+
+/*
+class 1abc{
+}
+*/
+
+class Public{
+}
+
+class publicstatic{
+}
+
+class BiaoShiFuTest{
+}
