f91db8d569f4c97a024d390b9c4a22cb53490185
kleeeeea <klee@cs.ucsb.edu>
2024-12-01 15:51:08 -0800

    x

--- no_ide/HelloWorld.java
+++ no_ide/HelloWorld.java
@@ -1,19 +1,7 @@
-
-//
-//import org.joda.time.LocalTime;
-//
-//public class HelloWorld {
-//    public static void main(String[] args) { LocalTime currentTime = new LocalTime();
-//		  System.out.println("keqian's log: The current local time is: " + currentTime);
-//
-//        Greeter greeter = new Greeter();
-//        System.out.println(greeter.sayHello());
-//    }
-//}
-public class HelloWorld {
+class HelloChina{
 	public static void main(String[] args){
-		System.out.println("hello,world!!aaakeqianhh");
-		System.out.print("hello,world!!ï¿½ï¿½Ã£ï¿½ï¿½Ð¹ï¿½ï¿½ï¿½");
+		System.out.println("hello,world!!ÄãºÃ£¬ÖÐ¹ú£¡");
+		System.out.print("hello,world!!ÄãºÃ£¬ÖÐ¹ú£¡");
 		System.out.println("123abc");
 		System.out.println(123 + 1);
 	}
